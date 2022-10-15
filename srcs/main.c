1c8b6266e022006ea94647c78a7e08f3a514124e
Alexis GENGEMBRE <agengemb@e1r6p6.clusters.42paris.fr>
2022-10-15 20:00:36 +0200

    squelette4

--- srcs/main.c
+++ srcs/main.c
@@ -6,9 +6,22 @@
 /*   By: agengemb <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 18:55:30 by agengemb          #+#    #+#             */
-/*   Updated: 2022/10/15 19:46:00 by agengemb         ###   ########.fr       */
+/*   Updated: 2022/10/15 19:58:46 by agengemb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stddef.h>
+#include "../includes/stack.h"
+
+size_t	ft_strlen(char	*s)
+{
+	size_t size;
+
+	size = 0;
+	while (*(s + size))
+		size++;
+	return (size);
+}
 
 /*
  *Description:
@@ -30,11 +43,13 @@ int	check_argv(int argc, char **argv)
 	while (i < argc)
 	{
 		j = 0;
+		if (!ft_strlen(argv[i]))
+			return (0);
 		if (argv[i][j] == '-')
 			j = 1;
 		while (argv[i][j])
 		{
-			if (!is_digit(argv[i][j]))
+			if (!ft_isdigit(argv[i][j]))
 				return (0);
 			j++;
 		}
@@ -46,11 +61,10 @@ int	check_argv(int argc, char **argv)
 int main(int argc, char **argv)
 {
 	if (argc <= 1 || !check_argv(argc - 1, argv + 1))
-		return (0);
-	t_stack *a = ft_stacknew();
+		return (0);	
+ 	t_stack *a = ft_stacknew();
 	t_stack *b = ft_stacknew();
 	if (!a || !b)
 		return (0);
-	
 	return (0);
 }
